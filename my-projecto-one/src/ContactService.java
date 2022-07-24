39920b2f07717a6c9029686f4c31ce7b541f362b
JBlades10 <pixeloso0527@gmail.com>
2022-07-24 10:16:12 -0400

    assign a unique ID of exactly 10 char

--- my-projecto-one/src/ContactService.java
+++ my-projecto-one/src/ContactService.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class ContactService {
-
+	private String uniqueID;
+	private List<Contact> contactList = new ArrayList<>();
+	
+	{
+		// assigns a unique ID of exactly 10 char
+		uniqueID = UUID.randomUUID().toString().substring(0,10);
+	}
 }
