bc326428e3fe4416041eb30ecd1d5c04862ba0b4
JBlades10 <pixeloso0527@gmail.com>
2022-07-24 11:06:51 -0400

    create the searchForContact by id,
    And deleteContact by id methods.

--- my-projecto-one/src/ContactService.java
+++ my-projecto-one/src/ContactService.java
@@ -10,9 +10,24 @@ public class ContactService {
 	private String newUniqueID() {
 		return uniqueID = UUID.randomUUID().toString().substring(0,10);
 	}
-	//add new contact
+	//Search for contact by ID
+	private Contact searchForContact(String id) throws Exception {
+		int index = 0;
+		while (index < contactList.size()) {
+			if(id.equals(contactList.get(index).getContactID())) {
+				return contactList.get(index);
+			}
+			index ++;
+		}
+		throw new Exception ("The contact does not exists");
+	}
+	//Add new contact
 	public void newContact() {
 		Contact contact = new Contact(newUniqueID());
 		contactList.add(contact);
+	}
+	//Delete a contact by ID
+	public void deleteContact(String id) throws Exception{
+		contactList.remove(searchForContact(id));
 	}
 }
