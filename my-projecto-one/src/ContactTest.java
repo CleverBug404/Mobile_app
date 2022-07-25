961fb25bc8971962fe5343ed62cc84c65e031c0c
JBlades10 <pixeloso0527@gmail.com>
2022-07-25 17:18:19 -0400

    Created ContacTest file

---
+++ my-projecto-one/src/ContactTest.java
@@ -0,0 +1,50 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class ContactTest {
+	protected String contactID, firstNameTest, lastNameTest, phoneNumberTest, addressTest;
+	protected String longContactID, longFirstNameTest, longLastNameTest, longPhoneNumberTest, longAddressTest;
+	
+	@BeforeEach
+	void setUp() {
+		contactID ="ABCDE12345";
+		firstNameTest ="Jack";
+		lastNameTest ="Blades";
+		phoneNumberTest ="7867736495";
+		addressTest ="3322 Olympic Drive Alvion";
+		longContactID = "112233445566778899";
+		longFirstNameTest ="TheresaRosalia";
+		longLastNameTest ="TheNightOfSpades";
+		longPhoneNumberTest ="77733389651";
+		longAddressTest ="Knothole Island North Of Albion by the Spire";
+	}
+	
+	@Test
+	void contactTest() {
+		Contact contact = new Contact();
+		assertAll("Constructor", 
+				() -> assertNotNull(contact.getContactID()),
+				() -> assertNotNull(contact.getFirstName()),
+				() -> assertNotNull(contact.getLastName()),
+				() -> assertNotNull(contact.getPhoneNumber()),
+				() -> assertNotNull(contact.getAddress())
+				);
+	}
+	
+	@Test
+	void constantIDConstructorTest() {
+		Contact contact = new Contact(contactID);
+		assertAll("Constructor ID",
+				() -> assertEquals(contactID, contact.getContactID()),
+				() -> assertNotNull(contact.getFirstName()),
+				() -> assertNotNull(contact.getLastName()),
+				() -> assertNotNull(contact.getPhoneNumber()),
+				() -> assertNotNull(contact.getAddress())
+				
+				);
+	}
+
+}
