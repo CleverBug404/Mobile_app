c9e7617eb326153931ede402b93e4dd4c47b5bb5
JBlades10 <pixeloso0527@gmail.com>
2022-07-30 11:40:47 -0400

    Create setters and checks for the Task class.

--- my-projecto-one/src/Task.java
+++ my-projecto-one/src/Task.java
@@ -23,4 +23,32 @@ public class Task {
 		return taskDescription;
 	}
 	
+	
+	//check
+	private void checkTaskID(String id) {
+		if (id.length() > 10 || id == null) {
+			throw new IllegalArgumentException("The task ID cannot be longer han 10 characters, or be null");
+		} else {
+			this.taskID = id;
+		}
+	}
+	//setters
+	protected void setTaskName(String name) {
+		if (name.length() > 20 || name == null) {	
+			throw new IllegalArgumentException("Task name cannot be longer than 20 characters, or be null");
+		}else {
+			this.taskName = name;
+		}
+	}
+	protected void setTaskDescription(String description) {
+		if (description.length() > 50 || description == null) {
+			throw new IllegalArgumentException("The task description cannot be longer rhan 50 characters, or be null");
+		} else {
+			this.taskDescription = description;
+		}
+	}
+	
+	//defining task object
+	
+
 }
